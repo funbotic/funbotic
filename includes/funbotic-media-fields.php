8d47b0859c13413b6bf924f44de2ee42d2c9673c
unknown <alabrie@funbotlab.com>
2018-04-11 17:48:05 -0400

    Cleaned up media fields, removed old file.

--- includes/funbotic-media-fields.php
+++ includes/funbotic-media-fields.php
@@ -102,11 +102,13 @@ function funbotic_update_value_campers_in_media( $value, $field, $post_id ) {
 
 	}
 	
+	/*
 	// Test
 	update_post_meta( 4406, 'test_previous_campers', $previously_associated_campers );
 	update_post_meta( 4406, 'test_current_campers', $current_associated_campers );
 	update_post_meta( 4406, 'test_new_campers', $new_campers );
 	update_post_meta( 4406, 'test_campers_to_remove', $campers_to_remove );
+	*/
 
 	// Process each new camper.  Add the ID of this image to their user_meta.
 	foreach( $new_campers as $new_camper ) {
@@ -148,106 +150,4 @@ function funbotic_clean_array( array $array_in ) {
 	$return = array();
 	array_walk_recursive( $array_in, function($a) use (&$return) { $return[] = $a; } );
 	return $return;
-}
-
-
-
-
-function funbotic_apply_media_fields_filter( $form_fields, $post = null ) {
-	// This is used to ensure each camper's checkbox has a unique ID associated with it.
-	$camper_counter = 1;
-
-    // If the post in question is an image.
-    if ( preg_match( "/" . 'image' . "/", $post->post_mime_type ) ) {
-		// Get saved metadata of currently associated images.
-		$current_associated_images = get_post_meta( $post->ID, 'funbotic_associated_images', true );
-		
-		// Generate current list of all campers.
-		$camper_list = funbotic_generate_camper_data_array();
-
-		?>
-		<form action="/funbotic-media-fields.php" method="post">
-		<div><label for="camper_selection">Please select all campers present in this image.  This image will show up in the camper\'s private profile, so please ensure they are clearly visible.</label></div>
-		<?php
-		// For each camper, determine if they are already associated with this image, then generate html to create a checkbox that is checked or unchecked, as appropriate.
-		foreach ( $camper_list as $camper ) {
-			$string_array = explode('(' , rtrim($camper, ')'));
-			(int) $current_ID = $string_array[1];
-			
-			if ( in_array($current_ID, $current_associated_images) ) {
-				$checked = ' checked="checked"';
-			} else {
-				$checked = '';
-			} // End if/else.
-			
-			// Create array of checkboxes for ease of processing when saving data, hence the usage of camper_selection[] as name.
-			?>
-			<div><input<?php echo esc_attr( $checked )?> type="checkbox" name="camper_selection[]" value="<?php echo esc_attr( $camper )?>">
-			<label><?php echo esc_attr( $camper )?></label></div>
-			<?php
-
-			$camper_counter++;
-		}
-		?>
-		</form>
-		<?php
-	
-    // If the post in question is a video.
-    } elseif ( preg_match( "/" . 'video' . "/", $post->post_mime_type ) ) {
-        $meta = get_post_meta( $post->ID, 'funbotic_associated_videos', true );
-
-	} // End elseif.
-} // End function funbotic_apply_filter.
-
-
-function funbotic_save_media_fields( $post, $attachment ) {
-	// If the post in question is an image.
-    //if ( preg_match( "/" . 'image' . "/", $post->post_mime_type ) ) { <-- PROBLEM LINE
-		//update_post_meta( $post['ID'], 'funbotic_associated_images', 'test2 succesful' );
-		// Get saved metadata of currently associated images.
-		$current_associated_images = get_post_meta( $post->ID, 'funbotic_associated_images', true );
-
-		if ( !empty( $_POST['camper_selection'] ) ) {
-			foreach ( $_POST['camper_selection'] as $selected ) {
-				update_post_meta( $post['ID'], '_image_subject', $selected->value );
-				$string_array = explode('(' , rtrim($selected, ')'));
-				(int) $current_ID = $string_array[1];
-
-				if ( in_array( $current_ID, $current_associated_images ) ) {
-					// Do nothing, as the current ID is already saved as a currently associated image.
-				} else {
-					array_push( $current_associated_images, $current_ID );
-				} // End if/else.
-			} // End foreach.
-		//} // End if.
-		update_post_meta( $post['ID'], 'funbotic_associated_images', $current_associated_images );
-
-	// If the post in question is a video.
-    } elseif ( preg_match( "/" . 'video' . "/", $post->post_mime_type ) ) {
-        $meta = get_post_meta( $post->ID, 'funbotic_associated_videos', true );
-
-	} // End elseif.
-} // End function funbotic_save_media_fields.
-
-
-// Generates an array consisting of the display name of a camper and their ID, to be used as a list of options when
-// selecting which camper is featured in a piece of media.  ID can be extracted from the string when needed.
-function funbotic_generate_camper_data_array() {
-	$args = array(
-		'role' 		=> 'subscriber',
-		'orderby' 	=> 'display_name',
-		'order'		=> 'ASC',
-	);
-
-	$camper_data_array = get_users( $args );
-
-	$return_array = array();
-
-	foreach ( $camper_data_array as $camper ) {
-		$camper_ID = $camper->ID;
-		$camper_display_name = $camper->display_name;
-		$camper_string = $camper_display_name . " (" . $camper_ID . ")";
-		array_push( $return_array, $camper_string );
-	}
-	return $return_array;
 }
