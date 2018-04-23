b5a140545413a7c617cc035faf646726e9b944fa
Alexander LaBrie <alabrie@funbotlab.com>
2018-04-23 19:44:34 -0400

    Beginnings of parent relationship.

--- includes/funbotic-parent-child-relationships.php
+++ includes/funbotic-parent-child-relationships.php
@@ -14,10 +14,52 @@
  * Creates relationships between user accounts to denote parent/child relationships, giving the ability for parents to monitor their children's progress.
  */
 
+// Advanced Custom Fields load field filter, to allow for spontaneous generation of potential parent names.
+add_filter( 'acf/load_field/name=funbotic_parents', 'funbotic_load_parents' );
+
 // Advanced Custom Fields load field filter, to allow for spontaneous generation of potential child names.
-add_filter( 'acf/load_field/name=funbotic_children', 'funbotic_load_campers_in_media' );
+add_filter( 'acf/load_field/name=funbotic_children', 'funbotic_load_children' );
 // Filter before values are saved in database.
 add_filter( 'acf/update_value/name=funbotic_children', 'funbotic_update_value_funbotic_children', 10, 3 );
+
+
+// Same idea as funbotic_load_campers_in_media, from funbotic-media-fields.php.
+function funbotic_load_parents( $field ) {
+
+	$args = array(
+		'role' 		=> 'customer',
+		'orderby' 	=> 'display_name',
+		'order'		=> 'ASC',
+	);
+
+	$parent_data_array = get_users( $args );
+	
+	// Clear choices array in case it was previously set.
+	$field['choices'] = array();
+
+	foreach ( $parent_data_array as $parent ) {
+		$parent_ID = $parent->ID;
+		$parent_display_name = $parent->display_name;
+		$field['choices'][$parent_ID] = $parent_display_name;
+	}
+
+	$user_id = (int) $user_id;
+	// This appears to be the only way to properly get the values from the field, as
+	// dynamically generated checkboxes don't have a 'values' array, merely a 'choices' array at this stage.
+	$previously_associated_parents = get_user_meta( $user_id, 'funbotic_parents' );
+	// We need to make sure to save the parents who are associated with this user BEFORE any changes
+	// are made to it, otherwise we will not be able to accurately compare changes when updating values.
+
+	if ( empty( $previously_associated_parents ) || is_null( $previously_associated_parents ) ) {
+		update_user_meta( $user_id, 'funbotic_previously_associated_parents', $previously_associated_parents );
+	} else {
+		$new_meta = funbotic_clean_array( $previously_associated_parents );
+		update_user_meta( $user_id, 'funbotic_previously_associated_parents', $new_meta );
+	}
+
+	return $field;
+
+}
 
 
 // Same idea as funbotic_load_campers_in_media, from funbotic-media-fields.php.
