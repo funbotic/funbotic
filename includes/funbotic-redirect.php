37d795bfc9da5a4e3ebe47073b7f5693de3bc7b3
Alexander LaBrie <AlexanderLaBrie1985@gmail.com>
2018-06-01 19:29:14 -0400

    wp-admin login now also takes users to Profile page.

--- includes/funbotic-redirect.php
+++ includes/funbotic-redirect.php
@@ -10,7 +10,17 @@
  * @author     Alexander LaBrie <alabrie@funbotlab.com>
  */
 
+/**
+ * Redirect users to the Profile page upon login, and back to the Home page upon logging out.
+ */
+
+add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
 add_action('wp_logout','funbotic_auto_redirect_after_logout');
+
+function my_login_redirect( $redirect_to, $request, $user ) {
+    $redirect_to = home_url() . '/my-account/';
+    return $redirect_to;
+}
 
 function funbotic_auto_redirect_after_logout() {
     wp_redirect( home_url() );
