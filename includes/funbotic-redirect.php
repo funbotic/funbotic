9c868649242bc3e2fe859496ef191532300598d6
Alexander LaBrie <AlexanderLaBrie1985@gmail.com>
2018-07-16 19:24:05 -0400

    Reset password redirects to /lost-password page.

--- includes/funbotic-redirect.php
+++ includes/funbotic-redirect.php
@@ -11,16 +11,24 @@
  */
 
 /**
- * Redirect users to the Profile page upon login, and back to the Home page upon logging out.
+ * Redirect users to the Profile page upon login, and back to the Home page upon logging out.  Redirects to a custom /lost-password page.
  */
 
-add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
+add_filter( 'login_redirect', 'funbotic_login_redirect', 10, 3 );
+add_filter('lostpassword_url', 'funbotic_lost_password_redirect');
 add_action('wp_logout','funbotic_auto_redirect_after_logout');
 
-function my_login_redirect( $redirect_to, $request, $user ) {
+
+function funbotic_login_redirect( $redirect_to, $request, $user ) {
     $redirect_to = home_url() . '/my-account/';
     return $redirect_to;
 }
+
+
+function funbotic_lost_password_redirect() {
+    return home_url('/lost-password');
+}
+
 
 function funbotic_auto_redirect_after_logout() {
     wp_redirect( home_url() );
