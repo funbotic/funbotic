f93bd4f7c4abdabb3d37cec2500f3227ec364b44
Alexander LaBrie <AlexanderLaBrie1985@gmail.com>
2018-07-30 20:51:58 -0400

    Beginning work on pulling another user's LD data.  Added function.

--- includes/funbotic-dynamic-user-gallery.php
+++ includes/funbotic-dynamic-user-gallery.php
@@ -52,6 +52,114 @@ function funbotic_user_gallery_init() {
 } // End function funbotic_user_gallery_init.
 
 
+/**
+ * Code copied from LearnDash's ld-course-info-widget.php, now attempting to modify to display user of choice with added parameter.
+ * 
+ * @since 1.1.5
+ * 
+ * @param  array 	$atts 	shortcode attributes
+ * @return string 	output profile for user
+ */
+function funbotic_learndash_profile( $atts, $user_to_get ) {
+	global $learndash_shortcode_used;
+	
+	// Add check to ensure user is logged in.
+	if ( !is_user_logged_in() ) return '';
+	
+	$defaults = array(
+		'user_id'				=>	$user_to_get,
+		'per_page'				=>	false,
+		'order' 				=> 'DESC', 
+		'orderby' 				=> 'ID', 
+		'course_points_user' 	=> 'yes',
+		'expand_all'			=> false
+	);
+	$atts = wp_parse_args( $atts, $defaults );
+
+	if ( ( strtolower($atts['expand_all'] ) == 'yes' ) || ( $atts['expand_all'] == 'true' ) || ( $atts['expand_all'] == '1' ))
+		$atts['expand_all'] = true;
+	else
+		$atts['expand_all'] = false;
+
+	if ( $atts['per_page'] === false ) {
+		$atts['per_page'] = $atts['quiz_num'] = LearnDash_Settings_Section::get_section_setting( 'LearnDash_Settings_Section_General_Per_Page', 'per_page' );
+	} else {
+		$atts['per_page'] = intval( $atts['per_page'] );
+	}
+
+	if ( $atts['per_page'] > 0 ) {
+		$atts['paged'] = 1;
+	} else {
+		unset( $atts['paged'] );
+		$atts['nopaging'] = true;
+	}
+
+	$atts = apply_filters('learndash_profile_shortcode_atts', $atts);
+
+	if ( empty( $atts['user_id'] ) ) return;
+
+	$current_user = get_user_by( 'id', $atts['user_id'] );
+	$user_courses = ld_get_mycourses( $atts['user_id'], $atts );
+
+	$usermeta = get_user_meta( $atts['user_id'], '_sfwd-quizzes', true );
+	$quiz_attempts_meta = empty( $usermeta ) ? false : $usermeta;
+	$quiz_attempts = array();
+
+	if ( ! empty( $quiz_attempts_meta ) ) {
+
+		foreach ( $quiz_attempts_meta as $quiz_attempt ) {
+			$c = learndash_certificate_details( $quiz_attempt['quiz'], $atts['user_id'] );
+			$quiz_attempt['post'] = get_post( $quiz_attempt['quiz'] );
+			$quiz_attempt['percentage'] = ! empty( $quiz_attempt['percentage'] ) ? $quiz_attempt['percentage'] : ( ! empty( $quiz_attempt['count'] ) ? $quiz_attempt['score'] * 100 / $quiz_attempt['count'] : 0 );
+			
+			if ( $atts['user_id'] == get_current_user_id() && ! empty( $c['certificateLink'] ) && ( ( isset( $quiz_attempt['percentage'] ) && $quiz_attempt['percentage'] >= $c['certificate_threshold'] * 100 ) ) ) {
+				$quiz_attempt['certificate'] = $c;
+			}
+
+			if ( !isset( $quiz_attempt['course'] ) )
+				$quiz_attempt['course'] = learndash_get_course_id( $quiz_attempt['quiz'] );
+			$course_id = intval( $quiz_attempt['course'] );
+
+			$quiz_attempts[$course_id][] = $quiz_attempt;
+		}
+	}
+	
+	$profile_pager = array();
+	
+	if ( ( isset( $atts['per_page'] ) ) && ( intval( $atts['per_page'] ) > 0 ) ) {
+		$atts['per_page'] = intval( $atts['per_page'] );
+			
+		//$paged = get_query_var( 'page', 1 );
+		//error_log('paged['. $paged .']');
+		
+		if ( ( isset( $_GET['ld-profile-page'] ) ) && ( !empty( $_GET['ld-profile-page'] ) ) ) {
+			$profile_pager['paged'] = intval( $_GET['ld-profile-page'] );
+		} else {
+			$profile_pager['paged'] = 1;
+		}
+		
+		$profile_pager['total_items'] = count( $user_courses );
+		$profile_pager['total_pages'] = ceil( count( $user_courses ) / $atts['per_page'] );
+		
+		$user_courses = array_slice ( $user_courses, ( $profile_pager['paged'] * $atts['per_page'] ) - $atts['per_page'], $atts['per_page'], false );
+	}
+	
+	$learndash_shortcode_used = true;
+
+	return SFWD_LMS::get_template( 
+		'profile', 
+		array(
+			'user_id' 			=> 	$atts['user_id'], 
+			'quiz_attempts' 	=> 	$quiz_attempts, 
+			'current_user' 		=> 	$current_user, 
+			'user_courses' 		=> 	$user_courses,
+			'shortcode_atts'	=>	$atts,
+			'profile_pager'		=>	$profile_pager
+		) 
+	);
+}
+
+
 function funbotic_parent_display_init() {
     function funbotic_generate_parent_display( $content = null ) {
         $id = get_current_user_id();
